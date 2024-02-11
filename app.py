87cddf1ad7ccaaf1521d9f85c7415410183f67e9
semlie <lieb@DESKTOP-3L46V51>
2024-02-11 20:26:23 +0200

    Add Flask app and requirements.txt

---
+++ app.py
@@ -0,0 +1,14 @@
+
+from os import name
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+
+    return jsonify(dict(name='John', age=25, city='New York'))
+
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', port=8000,  debug=True)
