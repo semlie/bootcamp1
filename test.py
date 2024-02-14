d441d628de6caec65880b008e5568d892ce76f7f
lidorAvisar <avisar0402@gmail.com>
2024-02-14 21:00:56 +0200

    complete_func

--- test.py
+++ test.py
@@ -1,12 +1,25 @@
-import os
-import pickle
+def check_intent(text):
+    if 'buy' in text.lower() and 'sell' in text.lower():
+        return 'both buying and selling'
+    elif 'buy' in text.lower():
+        return 'buying'
+    elif 'sell' in text.lower():
+        return 'selling'
+    else:
+        return 'none of them!'
 
-file_path = '../data/filterd.txt'
 
-if os.path.exists(file_path):
-    with open(file_path, 'rb') as file:
-        obj = pickle.load(file)
-    print(obj)
-else:
-    print("File not found:", file_path)
+text1 = "I want to buy a new phone and I have some items for sell"
+text2 = "I am looking for a laptop to buy"
+text3 = "I have some furniture for sale"
+text4 = "I want to sell my car"
 
+intent1 = check_intent(text1)
+intent2 = check_intent(text2)
+intent3 = check_intent(text3)
+intent4 = check_intent(text4)
+
+print("Intent 1:", intent1)
+print("Intent 2:", intent2)
+print("Intent 3:", intent3)  
+print("Intent 4:", intent4) 
