57479a9ac099bf3df0668f5a24cd7d037f098bd0
OrAboudi <oriaboudi2001@gmail.com>
2024-02-18 09:22:02 +0200

    imp recognize items brand and index order by regex.

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from os import name
-from service.dataManipulation import extract_data
+from service.dataManipulation import clean_smartphone_list
+from flask import Flask, jsonify
 
-from flask import Flask
 app = Flask(__name__)
-
 
 @app.route('/')
 def products():
-
-    return extract_data()
+    data = clean_smartphone_list()
+    return data
 
 if __name__ == '__main__':
-    app.run('127.0.0.1', port=8000,  debug=True)
+    app.run('127.0.0.1', port=8000, debug=True)
--- service/dataManipulation.py
+++ service/dataManipulation.py
@@ -17,10 +17,16 @@ def extract_data():
     # Decode utf-8 bytes to string
     text = utf8_text.decode('utf-8')
 
-    # Flatten the text by removing numbers and words with uppercase letters
-  
 
-# Define a regular expression pattern to match smartphone names
+    return text
+    
+
+
+def create_smart_phone_list():
+
+    text = extract_data()
+
+    # Define a regular expression pattern to match smartphone names
     pattern = r'\b[A-Z][a-zA-Z0-9\s-]+\b'
 
     # Find all matches of the pattern in the text
@@ -29,9 +35,33 @@ def extract_data():
     # Filter out common words that are not smartphone names
     filtered_matches = [match for match in matches if len(match) > 3]
 
-    # Output the extracted smartphone names
-    for name in filtered_matches:
-        print(name)
-    return name
+    # Create a list of dictionaries to hold the extracted smartphone names with IDs
+    smartphone_list = []
+    
+    # Assign IDs to filtered matches and store them in the list of dictionaries
+    for idx, name in enumerate(filtered_matches, start=1):
+        smartphone_list.append({"id": idx, "brand": name})
 
+    return smartphone_list
+
+
+
+def clean_smartphone_list():
+
+    smartphone_list = create_smart_phone_list()
+
+    cleaned_smartphone_list = []
+    for smartphone in smartphone_list:
+        cleaned_brand = smartphone['brand']
+        # Remove \n
+        cleaned_brand = re.sub(r'\n', '', cleaned_brand)
+        # Remove \r
+        cleaned_brand = re.sub(r'\r', '', cleaned_brand)
+        # Remove \t
+        cleaned_brand = re.sub(r'\t', '', cleaned_brand)
+        # Remove numbers with more than 3 digits
+        cleaned_brand = re.sub(r'\b\d{4,}\b', '', cleaned_brand)
+        
+        cleaned_smartphone_list.append({"id": smartphone["id"], "brand": cleaned_brand})
+    return cleaned_smartphone_list
 
