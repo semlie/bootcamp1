1a45e34af17fd6de2bf2cb2491d1118e0d74b7bc
nadavHD <nadavyona70@gmail.com>
2024-02-20 20:01:59 +0200

    #30 done

---
+++ pickleRead.py
@@ -0,0 +1,19 @@
+import pickle
+
+def open_pickle_file(file_path):
+  
+  # Open the file in binary read mode
+  with open(file_path, "rb") as f:
+    try:
+      # Load the data
+      data = pickle.load(f)
+      return data
+    except EOFError:
+      raise IOError("Pickle file is empty.")
+    except pickle.UnpicklingError:
+      raise IOError("Error unpickling data.")
+
+# Example usage
+file_path ="data/filterd.pkl"
+data = open_pickle_file(file_path)
+print(f"Loaded data from pickle file: {data}")
