212dc334b24e030598289a9cf39c4cc1b9d66f8d
OrAboudi <oriaboudi2001@gmail.com>
2024-02-24 20:23:31 +0200

    update annotator generator with real data

--- service/annotator.py
+++ service/annotator.py
@@ -20,8 +20,8 @@ def generate_text_annotator():
         text_data = utf8_text.decode('utf-8')
 
     # Sample array of brands and colors
-    brands = ['Samsung', 'Apple', 'Huawei']
-    colors = ['graph', 'violet', 'blue']
+    brands = get_brand()
+    colors = get_color()
 
     # Function to check if a word exists in the array
     def check_word(word, word_list):
@@ -51,7 +51,7 @@ def generate_text_annotator():
             start_index = text_data.find(line)
             
             if brand_found:
-                
+
                 wrod_in_line = line.find(brand)
                 start_index_brand = start_index + wrod_in_line
                 end_index_brand = start_index_brand + len(brand)
@@ -62,19 +62,239 @@ def generate_text_annotator():
                 start_index_color = start_index + wrod_in_line
                 end_index_color = start_index_color + len(color)
 
+            # # Create annotation dictionary
+            # annotation = {
+            #     "entities": [
+            #         [start_index_brand, end_index_brand, "BRAND"] if brand_found else 
+            #         [start_index_color, end_index_color, "COLOR"] if color_found else 
+            #     ]
+            # }
+            # annotations.append([line, annotation])
+                
             # Create annotation dictionary
-            annotation = {
-                "entities": [
-                    [start_index_brand, end_index_brand, "BRAND"] if brand_found else None,
-                    [start_index_color, end_index_color, "COLOR"] if color_found else None
-                ]
-            }
-            annotations.append([line, annotation])
+            annotation_entities = []
+            if brand_found:
+                 annotation_entities.append([start_index_brand, end_index_brand, "BRAND"])
+            if color_found:
+                 annotation_entities.append([start_index_color, end_index_color, "COLOR"])
+
+        # Add the annotation only if at least one entity is found
+            if annotation_entities:
+                annotation = {"entities": annotation_entities}
+                annotations.append([line, annotation])
 
     # Write annotations to JSON file
     with open('annotations.json', 'w') as f:
         json.dump(annotations, f, indent=4)
 
     print("Annotations saved to annotations.json")
+
+
+def get_brand():
+    return  [
+        "Apple",
+        "Samsung",
+        "Huawei",
+        "Xiaomi",
+        "OnePlus",
+        "Oppo",
+        "Vivo",
+        "Google Pixel",
+        "Sony Xperia",
+        "Motorola",
+        "LG",
+        "HTC",
+        "Nokia",
+        "Asus",
+        "BlackBerry",
+        "Lenovo",
+        "ZTE",
+        "Meizu",
+        "Realme",
+        "Honor",
+        "Sharp",
+        "Panasonic",
+        "TCL",
+        "Alcatel",
+        "LeEco",
+        "Ulefone",
+        "Elephone",
+        "Coolpad",
+        "Gionee",
+        "Micromax",
+        "Infinix",
+        "Tecno",
+        "iQOO",
+        "Redmi",
+        "Poco",
+        "Itel",
+        "Doogee",
+        "Cubot",
+        "Vernee",
+        "Vivo",
+        "Umidigi",
+        "Wileyfox",
+        "Energizer",
+        "Razer",
+        "Nextbit",
+        "Essential",
+        "Fairphone",
+        "Black Shark",
+        "Sharp Aquos",
+        "Bluboo",
+        "Wiko",
+        "Meitu",
+        "Vodafone",
+        "YotaPhone",
+        "Sirin Labs",
+        "Vertu",
+        "CAT (Caterpillar)",
+        "Google Nexus",
+        "HP",
+        "Dell",
+        "Acer",
+        "Fujitsu",
+        "Toshiba",
+        "Kyocera",
+        "Casio",
+        "NEC",
+        "Gigabyte",
+        "Panasonic Toughbook",
+        "Sonim",
+        "RugGear",
+        "Doogee",
+        "Homtom",
+        "F(x)tec",
+        "Planet Computers",
+        "Jolla",
+        "Fairphone",
+        "Unihertz",
+        "Energizer",
+        "NuAns",
+        "Punkt",
+        "Zebra",
+        "Bullitt",
+        "Crosscall",
+        "Land Rover Explore",
+        "Marshall",
+        "Palm",
+        "Shift Phones",
+        "Sugar",
+        "Turing",
+        "Silent Circle",
+        "Saygus",
+        "Obi Worldphone",
+        "Wileyfox",
+        "Archos",
+        "bq",
+        "Emporia",
+        "Gigaset",
+        "Hisense",
+        "Kodak",
+        "Yotaphone"
+        ]
+
+def get_color():
+    return [
+    "Black",
+    "White",
+    "Silver",
+    "Gold",
+    "Space Gray",
+    "Rose Gold",
+    "Midnight Black",
+    "Blue",
+    "Red",
+    "Green",
+    "Pink",
+    "Purple",
+    "Yellow",
+    "Coral",
+    "Azure",
+    "Mystic Bronze",
+    "Graphite",
+    "Emerald Green",
+    "Sunset Orange",
+    "Cloud White",
+    "Ocean Blue",
+    "Cosmic Gray",
+    "Aura Glow",
+    "Phantom Black",
+    "Frost White",
+    "Electric Blue",
+    "Lavender Purple",
+    "Pearl White",
+    "Titanium Gray",
+    "Platinum",
+    "Forest Green",
+    "Aurora",
+    "Ceramic Black",
+    "Royal Blue",
+    "Glacier White",
+    "Mint Green",
+    "Sunrise Red",
+    "Sapphire Blue",
+    "Burgundy Red",
+    "Amber Brown",
+    "Carbon Black",
+    "Deepsea Blue",
+    "Polar White",
+    "Sonic Blue",
+    "Blush Gold",
+    "Alpine Green",
+    "Copper",
+    "Lunar Gray",
+    "Shadow Black",
+    "Flamingo Pink",
+    "Brilliant Blue",
+    "Topaz Blue",
+    "Crimson Red",
+    "Opal Blue",
+    "Lagoon Blue",
+    "Ruby Red",
+    "Peacock Blue",
+    "Ice Blue",
+    "Champagne Gold",
+    "Moonlight Silver",
+    "Marble White",
+    "Pearl Black",
+    "Crystal Black",
+    "Arctic Silver",
+    "Rose Pink",
+    "Anthracite Gray",
+    "Ceramic White",
+    "Ruby Pink",
+    "Azure Blue",
+    "Sunburst Gold",
+    "Royal Gold",
+    "Phantom Blue",
+    "Amethyst Purple",
+    "Solar Red",
+    "Forest Mist Green",
+    "Cloud Blue",
+    "Flame Red",
+    "Topaz Gold",
+    "Rosewood Red",
+    "Quartz Pink",
+    "Sapphire Black",
+    "Metallic Blue",
+    "Midnight Blue",
+    "Brilliant Black",
+    "Peach Gold",
+    "Platinum Silver",
+    "Pebble Blue",
+    "Sunset Gold",
+    "Nebula Purple",
+    "Arctic Blue",
+    "Bordeaux Red",
+    "Mocha Brown",
+    "Deep Indigo",
+    "Carbon Fiber",
+    "Fog Gray",
+    "Bronze",
+    "Cherry Red",
+    "Metallic Red"
+    ]
+
 
 generate_text_annotator()
